c48381847e01a2780e26d732696d110f8f4d07c0
PatrickTobin15 <patrick.tobin@keyin.com>
2026-08-20 17:14:14 -0230

    Update OrderItemRepository.java

--- src/main/java/warehouse/com/system/repository/OrderItemRepository.java
+++ src/main/java/warehouse/com/system/repository/OrderItemRepository.java
@@ -1,1 +1,7 @@
+package com.warehouse.system.repository;
 
+import com.warehouse.system.entity.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+}
