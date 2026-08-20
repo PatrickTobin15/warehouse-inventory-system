684711787e82e8b3b2871599ef1d0c200b37202d
PatrickTobin15 <patrick.tobin@keyin.com>
2026-08-20 17:11:21 -0230

    Update OrderItemRequest.java

--- src/main/java/warehouse/com/system/dto/OrderItemRequest.java
+++ src/main/java/warehouse/com/system/dto/OrderItemRequest.java
@@ -1,1 +1,28 @@
+package com.warehouse.system.dto;
+
+// what the client will send for each item on a new order
+public class OrderItemRequest {
+
+    private Long productId;
+    private int quantity;
+
+    public OrderItemRequest() {
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
 
