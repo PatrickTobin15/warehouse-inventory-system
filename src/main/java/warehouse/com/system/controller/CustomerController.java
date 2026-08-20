349c10f67229c3018cd4f77332dae8cdbd7f4b06
PatrickTobin15 <patrick.tobin@keyin.com>
2026-08-20 10:02:09 -0230

    Update CustomerController.java

--- src/main/java/warehouse/com/system/controller/CustomerController.java
+++ src/main/java/warehouse/com/system/controller/CustomerController.java
@@ -1,1 +1,31 @@
+package com.warehouse.system.controller;
+
+import com.warehouse.system.entity.Customer;
+import com.warehouse.system.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+// Orders needed a customerId to be created, so this gives Postman a way to make one
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @GetMapping
+    public ResponseEntity<List<Customer>> getAllCustomers() {
+        return ResponseEntity.ok(customerService.getAllCustomers());
+    }
+
+    @PostMapping
+    public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer) {
+        Customer created = customerService.createCustomer(customer);
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
+    }
+}
 
