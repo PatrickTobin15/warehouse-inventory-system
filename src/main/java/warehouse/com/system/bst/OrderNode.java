cb48fdc34749778c233b7df5166f1662fea9369c
PatrickTobin15 <patrick.tobin@keyin.com>
2026-08-20 10:01:52 -0230

    Update OrderNode.java

--- src/main/java/warehouse/com/system/bst/OrderNode.java
+++ src/main/java/warehouse/com/system/bst/OrderNode.java
@@ -1,1 +1,14 @@
+package com.warehouse.system.bst;
 
+import com.warehouse.system.entity.Order;
+
+public class OrderNode {
+
+    Order data;
+    OrderNode left;
+    OrderNode right;
+
+    public OrderNode(Order data) {
+        this.data = data;
+    }
+}
