7c0aed0a7194d8ef9b74e2ee7a3649a75d385419
PatrickTobin15 <patrick.tobin@keyin.com>
2026-08-20 17:13:04 -0230

    Update OrderItem.java

--- src/main/java/warehouse/com/system/entity/OrderItem.java
+++ src/main/java/warehouse/com/system/entity/OrderItem.java
@@ -1,1 +1,71 @@
+package com.warehouse.system.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "order_items")
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int quantity;
+
+    // Multiple order items can point to the same product
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    // Multiple order items can belong to the same order
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    public OrderItem() {
+    }
+
+    public OrderItem(int quantity, Product product, Order order) {
+        this.quantity = quantity;
+        this.product = product;
+        this.order = order;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+}
