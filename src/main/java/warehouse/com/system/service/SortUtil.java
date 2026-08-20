ece2537b34173276b613129e14fa00ce4b14993f
PatrickTobin15 <patrick.tobin@keyin.com>
2026-08-20 17:20:48 -0230

    Update SortUtil.java

--- src/main/java/warehouse/com/system/service/SortUtil.java
+++ src/main/java/warehouse/com/system/service/SortUtil.java
@@ -1,1 +1,24 @@
+package com.warehouse.system.service;
 
+import com.warehouse.system.entity.Product;
+
+import java.util.Comparator;
+import java.util.List;
+
+// my one manual sorting algorithm insertion sort. no Collections.sort() or Stream.sorted() is anywhere within this coding that is here.
+public class SortUtil {
+
+    public static void insertionSort(List<Product> products, Comparator<Product> comparator) {
+        for (int i = 1; i < products.size(); i++) {
+            Product current = products.get(i);
+            int j = i - 1;
+
+            // shift everything bigger than the current one spot to the right
+            while (j >= 0 && comparator.compare(products.get(j), current) > 0) {
+                products.set(j + 1, products.get(j));
+                j--;
+            }
+            products.set(j + 1, current);
+        }
+    }
+}
