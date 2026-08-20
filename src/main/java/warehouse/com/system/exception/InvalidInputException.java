c1d725cf356d57590e3c438daba56dc9b4be00f8
PatrickTobin15 <patrick.tobin@keyin.com>
2026-08-20 17:13:44 -0230

    Update InvalidInputException.java

--- src/main/java/warehouse/com/system/exception/InvalidInputException.java
+++ src/main/java/warehouse/com/system/exception/InvalidInputException.java
@@ -1,1 +1,7 @@
+package com.warehouse.system.exception;
 
+public class InvalidInputException extends RuntimeException {
+    public InvalidInputException(String message) {
+        super(message);
+    }
+}
