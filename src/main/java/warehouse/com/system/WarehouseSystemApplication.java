a0d9037fac26562c70575ed3b6d7183345bc3193
PatrickTobin15 <patrick.tobin@keyin.com>
2026-08-20 17:21:05 -0230

    Update WarehouseSystemApplication.java

--- src/main/java/warehouse/com/system/WarehouseSystemApplication.java
+++ src/main/java/warehouse/com/system/WarehouseSystemApplication.java
@@ -1,1 +1,12 @@
+package com.warehouse.system;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarehouseSystemApplication.class, args);
+    }
+}
