ff5120d76019e39d02e354193fcfe18575187407
PatrickTobin15 <patrick.tobin@keyin.com>
2026-08-20 17:14:06 -0230

    Update CustomerRepository.java

--- src/main/java/warehouse/com/system/repository/CustomerRepository.java
+++ src/main/java/warehouse/com/system/repository/CustomerRepository.java
@@ -1,1 +1,7 @@
+package com.warehouse.system.repository;
 
+import com.warehouse.system.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
