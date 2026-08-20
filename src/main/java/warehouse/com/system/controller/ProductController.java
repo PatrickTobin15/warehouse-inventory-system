e1fbb55094df79e1c5cbe9ec4abf113c999d3197
PatrickTobin15 <patrick.tobin@keyin.com>
2026-08-20 10:03:37 -0230

    Update ProductController.java

--- src/main/java/warehouse/com/system/controller/ProductController.java
+++ src/main/java/warehouse/com/system/controller/ProductController.java
@@ -1,1 +1,35 @@
+package com.warehouse.system.controller;
+
+import com.warehouse.system.entity.Product;
+import com.warehouse.system.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts() {
+        return ResponseEntity.ok(productService.getAllProducts());
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+        Product created = productService.createProduct(product);
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
+    }
+
+    @GetMapping("/sorted")
+    public ResponseEntity<List<Product>> getSorted(@RequestParam String by) {
+        return ResponseEntity.ok(productService.getSortedProducts(by));
+    }
+}
 
